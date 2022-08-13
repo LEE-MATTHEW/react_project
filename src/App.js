ee7d4c49e880f1c657a2fe86ab05203c70612978
LEE-MATTHEW <gom5714@naver.com>
2022-08-13 23:11:03 +0900

    react_pratice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
+/* eslint-disable */
+
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '송도 맛집', '남자 신발 추천', '인천 데이트코스 추천']);
+  let posts = "강남 고기맛집"
+  let [따봉, 따봉변경] = useState(0);
+
+  function 제목변경() {
+    var newArray = [...글제목];
+    newArray[0] = '여자 코트 추천';
+    글제목변경(newArray);
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='black-nav'>
+        <div>개발 blog</div>
+      </div>
+      <div className='list'>
+        <h3>{글제목[0]}
+          <spun onClick={() => { 따봉변경(따봉 + 1) }}>👍
+          </spun>
+          {따봉}
+        </h3>
+        <p>2월 17일 발행</p>
+        <button onClick={ 제목변경 }>변경</button>
+        <hr />
+      </div>
+      <div className='list'>
+        <h3>{글제목[1]}</h3>
+        <p>3월 10일 발행</p>
+        <hr />
+      </div>
+      <div className='list'>
+        <h3>{글제목[2]}</h3>
+        <p>3월 19일 발행</p>
+        <hr />
+      </div>
+      <div className='list'>
+        <h3>{글제목[3]}</h3>
+        <p>3월 25일 발행</p>
+        <hr />
+      </div>
+
+      <Modal/>
     </div>
   );
+}
+
+function Modal() {
+  return(
+    <div className='modal'>
+        <h2>제목</h2>
+        <p>날짜</p>
+        <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
